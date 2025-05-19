f423540f9999af411c612e102eda286fc762fcac
Maiza Asif <maziaa@mun.ca>
2025-05-18 23:17:04 -0230

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings: [" Frontend Developer"," Web Developer"],
+    typeSpeed: 50,      // Lower = faster typing
+    backSpeed: 30,      // Lower = faster deleting
+    backDelay: 50,
+    loop:true
+});
